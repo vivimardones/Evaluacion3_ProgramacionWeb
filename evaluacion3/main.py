2746a0cd8704e9b6b3f2b1c3214b8b7149bcea09
vivimardones <des.viviana.mardones@gmail.com>
2024-11-17 23:51:59 -0300

    Se le da la logica al formulario 1

--- evaluacion3/main.py
+++ evaluacion3/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,8 +6,20 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/ejercicio1')
+@app.route('/ejercicio1', methods=['GET', 'POST'])
 def ejercicio1():
+    if request.method == 'POST':
+        nota1 = float(request.form['nota1'])
+        nota2 = float(request.form['nota2'])
+        nota3 = float(request.form['nota3'])
+        asistencia = float(request.form['asistencia'])
+        promedio = (nota1 + nota2 + nota3)/3
+        if promedio >= 40 and  asistencia >= 75 :
+            resultado = "Aprobado"
+        else :
+            resultado = "Reprobado"
+            
+        return render_template('ejercicio1.html', promedio=promedio, resultado = resultado)
     return render_template('ejercicio1.html')
 
 @app.route('/ejercicio2')
